3439bcf6c9ded6c56feb4c8b13b443c4a83e1082
Sion GENDERS <sion.genders@estiam.com>
2019-12-03 14:09:33 +0100

    Initial Commit

---
+++ StarWarsWPF/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using PokemonWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PokemonWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+
+        public List<Pokemon> pokemons = new List<Pokemon>();
+        public Pokemon SelectedPokemon = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            WebClient wc = new WebClient();
+            byte[] data = await wc.DownloadDataTaskAsync(new Uri("https://pokeapi.co/api/v2/pokemon/?limit=151"));
+            string json = Encoding.UTF8.GetString(data);
+            PokemonData result = JsonConvert.DeserializeObject<PokemonData>(json);
+
+            lb_pokemons.ItemsSource = result.Results;
+
+            pokemons = result.Results;
+
+        }
+
+        private void lb_pokemons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadDataAsync(pokemons[lb_pokemons.SelectedIndex].Url);
+        }
+
+        public async void LoadDataAsync(string url)
+        {
+            WebClient wc = new WebClient();
+            byte[] data = await wc.DownloadDataTaskAsync(new Uri(url));
+            string json = Encoding.UTF8.GetString(data);
+            Pokemon result = JsonConvert.DeserializeObject<Pokemon>(json);
+
+            SelectedPokemon = result;
+
+            UpdateUI(result);
+
+            //lb_pokemons.ItemsSource = result.Results;
+        }
+
+        public void UpdateUI(Pokemon p)
+        {
+            PokemonName.Text = p.Name;
+            PokemonImage.Source = new BitmapImage(new Uri(p.Sprites.front_default));
+        }
+    }
+}
---
+++ StarWarsWPF/Models/PokemonData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonWPF.Models
+{
+    public class PokemonData
+    {
+        public int Count { get; set; }
+        public string Next { get; set; }
+        public string Previous { get; set; }
+        public List<Pokemon> Results { get; set; }
+    }
+
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public String Url { get; set; }
+
+        public PokemonImgData Sprites { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+
+    public class PokemonImgData
+    {
+        public string front_default { get; set; }
+    }
+
+
+}
